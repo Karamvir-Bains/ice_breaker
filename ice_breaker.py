e969290402affc5b5749252c772ae797e6ab0fcc
Karamvir-Bains <kbains2000@gmail.com>
2023-05-21 14:33:35 -0600

    Run test chain

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,25 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+information = """
+somebody random
+"""
 if __name__ == "__main__":
     print("Hello LangChain")
+
+    summary_template = """
+        given the information {information} about a person I want you to create:
+        1. a short summary
+        2. two intresting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain.run(information=information))
